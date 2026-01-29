8cc9fe958621cc9bc401a34700b8218fb84e6e41
Abdullah Sarfraz <abdullahofficial77@gmail.com>
2026-01-29 13:24:46 +0500

    style(receipt): Add dashed separator after table header

--- src/utils/printer.js
+++ src/utils/printer.js
@@ -80,9 +80,9 @@ export const generateReceiptText = (bill, settings, width = '58mm', itemLayout =
         html += `<table style="width: 100%; border-collapse: collapse; font-size: 0.9em;">
             <thead>
                 <tr style="">
-                    <th style="${leftStyle} width: 45%; padding-bottom: 4px;">${l.item}</th>
-                    <th style="${centerStyle} width: 25%; padding-bottom: 4px;">${l.qty}</th>
-                    <th style="${rightStyle} width: 30%; padding-bottom: 4px;">${l.price}</th>
+                    <th style="${leftStyle} width: 45%; padding-bottom: 4px; border-bottom: 1px dashed black;">${l.item}</th>
+                    <th style="${centerStyle} width: 25%; padding-bottom: 4px; border-bottom: 1px dashed black;">${l.qty}</th>
+                    <th style="${rightStyle} width: 30%; padding-bottom: 4px; border-bottom: 1px dashed black;">${l.price}</th>
                 </tr>
             </thead>
             <tbody>`;
